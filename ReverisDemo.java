872de00e9bd196d334c494736a704f36d68c8b70
semere01 <semere_tereffe@protonmail.com>
2019-11-21 09:58:47 +0300

    Bug Fix: Demo ran endlessly.

--- ReverisDemo.java
+++ ReverisDemo.java
@@ -9,13 +9,20 @@ class ReverisDemo{
 
 		System.out.println("Welcome to your Reversi Game. Take a look at your board.");
 		game.showGame();
+
+
 		for(;;){
-		System.out.println("Player " + game.whoseTurn() + ", please make your move. Enter the x and y coordinates of the move you want to make.\n");
-		System.out.print("X: ");
-		x = in.nextInt();
-		System.out.print("Y: ");
-		y = in.nextInt();		
-		game.makeMove(x,y);
-		game.showGame();}
+			if (game.numOpenSquares == 0){
+				System.out.prinln("    GAME OVER!!!\n  Player " + game.NOTwhoseTurn() + " wins!!!!");
+				break;
+			}
+			System.out.println("Player " + game.whoseTurn() + ", please make your move. Enter the x and y coordinates of the move you want to make.\n");
+			System.out.print("X: ");
+			x = in.nextInt();
+			System.out.print("Y: ");
+			y = in.nextInt();		
+			game.makeMove(x,y);
+			game.showGame();
+		}
 	}
 }
